4e4d5b0c6318c9de2c37272e141ed1a1daddd5b2
shole <pahappo@gmail.com>
2013-04-12 16:05:30 +0300

    support for empty and closed tags

--- json2html-0.4.1.js
+++ json2html-0.4.1.js
@@ -11,18 +11,18 @@
 //  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 var json2html = {
-	
+
 	/* ---------------------------------------- Public Methods ------------------------------------------------ */
 	'transform': function(json,transform,_options) {
-		
+
 		//create the default output
 		var out = {'events':[],'html':''};
-		
+
 		//default options (by default we don't allow events)
 		var options = {
 			'events':false
 		};
-		
+
 		//extend the options
 		options = json2html._extend(options,_options);
 
@@ -31,19 +31,19 @@ var json2html = {
 
 			//Normalize strings to JSON objects if necessary
 			var obj = typeof json === 'string' ? JSON.parse(json) : json;
-			
+
 			//Transform the object (using the options)
 			out = json2html._transform(obj, transform, options);
 		}
-		
+
 		//determine if we need the events
 		// otherwise return just the html string
 		if(options.events) return(out);
 			else return( out.html );
 	},
-	
+
 	/* ---------------------------------------- Private Methods ------------------------------------------------ */
-	
+
 	//Extend options
 	'_extend':function(obj1,obj2){
 		var obj3 = {};
@@ -51,7 +51,7 @@ var json2html = {
 		for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
 		return obj3;
 	},
-	
+
 	//Append results
 	'_append':function(obj1,obj2) {
 		var out = {'html':'','event':[]};
@@ -63,18 +63,18 @@ var json2html = {
 
 		return(out);
 	},
-	
+
 	//Transform object
 	'_transform':function(json, transform, options) {
-		
+
 		var elements = {'events':[],'html':''};
-		
+
 		//Determine the type of this object
 		if(Array.isArray(json)) {
-			
+
 			//Itterrate through the array and add it to the elements array
 			var len=json.length;
-			for(var j=0;j<len;++j) {	
+			for(var j=0;j<len;++j) {
 				//Apply the transform to this object and append it to the results
 				elements = json2html._append(elements,json2html._apply(json[j], transform, j, options));
 			}
@@ -86,17 +86,17 @@ var json2html = {
 		}
 
 		//Return the resulting elements
-		return(elements);		
+		return(elements);
 	},
 
 	//Apply the transform at the second level
 	'_apply':function(obj, transform, index, options) {
 
 		var element = {'events':[],'html':''};
-		
+
 		//Itterate through the transform and create html as needed
 		if(Array.isArray(transform)) {
-			
+
 			var t_len = transform.length;
 			for(var t=0; t < t_len; ++t) {
 				//transform the object and append it to the output
@@ -105,21 +105,15 @@ var json2html = {
 
 		} else if(typeof transform === 'object') {
 
-			//Get the tag element of this transform
-			if( transform.tag !== undefined ) {
+			//Create a new object for the children
+			var children = {'events': [], 'html': ''};
 
-				//Create a new element
-				element.html += '<' + transform.tag;
-				
-				//Create a new object for the children
-				var children = {'events':[],'html':''};
-				
-				//innerHTML
-				var html;
+			//innerHTML
+			var html;
 
-				//Look into the properties of this transform
-				for (var key in transform) {
-
+			//Look into the properties of this transform
+			for (var key in transform) {
+				if (transform.hasOwnProperty(key)) {  // safe iteration
 					switch(key) {
 						case 'tag':
 							//Do nothing as we have already created the element from the tag
@@ -132,7 +126,7 @@ var json2html = {
 								//Apply the transform to the children
 								children = json2html._append(children,json2html._apply(obj, transform.children, index, options));
 							} else if(typeof transform.children === 'function') {
-								
+
 								//Get the result from the function
 								var temp = transform.children.call(obj, obj, index);
 
@@ -156,13 +150,17 @@ var json2html = {
 						break;
 
 						default:
+							// allow properties only if tag name given
+							if (transform.tag === undefined) {
+								break;
+							}
 							//Add the property as a attribute if it's not a key one
 							var isEvent = false;
-							
+
 							//Check if the first two characters are 'on' then this is an event
 							if( key.length > 2 )
 								if(key.substring(0,2).toLowerCase() == 'on') {
-									
+
 									//Determine if we should add events
 									if(options.events) {
 
@@ -173,7 +171,7 @@ var json2html = {
 											'data':options.eventData,
 											'index':index
 										};
-										
+
 										//create a new id for this event
 										var id = json2html._guid();
 
@@ -191,15 +189,15 @@ var json2html = {
 							if( !isEvent){
 								//Get the value
 								var val = json2html._getValue(obj, transform, key, index);
-								
+
 								//Make sure we have a value
                                 if(val !== undefined) {
                                     var out;
-                                    
+
                                     //Determine the output type of this value (wrap with quotes)
                                     if(typeof val === 'string') out = '"' + val.replace(/"/g, '&quot;') + '"';
                                     else out = val;
-                                    
+
                                     //creat the name value pair
 								    element.html += ' ' + key + '=' + out;
                                 }
@@ -207,21 +205,35 @@ var json2html = {
 						break;
 					}
 				}
+			}
 
-				//close the opening tag
-				element.html += '>';
-				
-				//add the innerHTML (if we have any)
-				if(html) element.html += html;
 
-				//add the children (if we have any)
-				element = json2html._append(element,children);
+			// check that tag name defined ( can be left undefined if you only want html or children )
+			if (transform.tag !== undefined) {
+				if (transform.tag[transform.tag.length - 1] !== '/') {
+					//Create a new element with properties
+					element.html = '<' + transform.tag + element.html + '>';
+				} else {
+					//Create a closed new element with properties
+					element.html = '<' + transform.tag.substring(0, transform.tag.length - 1) + element.html + ' />';
+				}
+			}
+
+			//add the innerHTML (if we have any)
+			if (html) element.html += html;
+
+			//add the children (if we have any)
+			element = json2html._append(element, children);
+
+			// check that tag is not an empty tag (  for example; 'tag':'br/'  )
+			if (transform.tag !== undefined && typeof transform.tag === 'string' && transform.tag[transform.tag.length - 1] !== '/') {
+				//if (typeof transform.tag === 'string' && transform.tag.indexOf('/') === -1) {
 
 				//add the closing tag
 				element.html += '</' + transform.tag + '>';
+
 			}
 		}
-		
 		//Return the output object
 		return(element);
 	},
@@ -236,12 +248,12 @@ var json2html = {
 
 	//Get the html value of the object
 	'_getValue':function(obj, transform, key,index) {
-		
+
 		var out = '';
-		
+
 		var val = transform[key];
 		var type = typeof val;
-		
+
 		if (type === 'function') {
 			return(val.call(obj,obj,index));
 		} else if (type === 'string') {
@@ -249,7 +261,7 @@ var json2html = {
 				/\$\{([^\}\{]+)\}/
 			],function( src, real, re ){
 				return real ? src.replace(re,function(all,name){
-					
+
 					//Split the string into it's seperate components
 					var components = name.split('.');
 
@@ -258,7 +270,7 @@ var json2html = {
 
 					//Output value
 					var outVal = '';
-					
+
 					//Parse the object components
 					var c_len = components.length;
 					for (var i=0;i<c_len;++i) {
@@ -270,14 +282,14 @@ var json2html = {
 							if(useObj === null || useObj === undefined) break;
 						}
 					}
-					
+
 					//As long as we have an object to use then set the out
 					if(useObj !== null && useObj !== undefined) outVal = useObj;
 
 					return(outVal);
 				}) : src;
 			});
-			
+
 			out = _tokenizer.parse(val).join('');
 		} else {
 			out = val;
@@ -285,13 +297,13 @@ var json2html = {
 
 		return(out);
 	},
-	
+
 	//Tokenizer
 	'_tokenizer':function( tokenizers, doBuild ){
 
 		if( !(this instanceof json2html._tokenizer ) )
 			return new json2html._tokenizer( tokenizers, doBuild );
-			
+
 		this.tokenizers = tokenizers.splice ? tokenizers : [tokenizers];
 		if( doBuild )
 			this.doBuild = doBuild;
@@ -305,29 +317,29 @@ var json2html = {
 			} while( !this.ended );
 			return this.tokens;
 		};
-		
+
 		this.build = function( src, real ){
 			if( src )
 				this.tokens.push(
 					!this.doBuild ? src :
 					this.doBuild(src,real,this.tkn)
-				);	
+				);
 		};
 
 		this.next = function(){
 			var self = this,
 				plain;
-				
+
 			self.findMin();
 			plain = self.src.slice(0, self.min);
-			
+
 			self.build( plain, false );
-				
+
 			self.src = self.src.slice(self.min).replace(self.tkn,function( all ){
 				self.build(all, true);
 				return '';
 			});
-			
+
 			if( !self.src )
 				self.ended = true;
 		};
@@ -336,7 +348,7 @@ var json2html = {
 			var self = this, i=0, tkn, idx;
 			self.min = -1;
 			self.tkn = '';
-			
+
 			while(( tkn = self.tokenizers[i++]) !== undefined ){
 				idx = self.src[tkn.test?'search':'indexOf'](tkn);
 				if( idx != -1 && (self.min == -1 || idx < self.min )){
